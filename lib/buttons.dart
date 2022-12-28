5d8b04df15d1ec4800a378f2ce0ff8a09c2132f8
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-12-27 18:52:34 -0800

    add code

---
+++ lib/buttons.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  final Function(String) changeJokeIndex;
+
+  Buttons({required this.changeJokeIndex});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(15, 5, 15, 5),
+          child: FloatingActionButton(
+            onPressed: () {
+              changeJokeIndex("prev");
+            },
+            child: Icon(Icons.arrow_left_rounded, size: 60),
+            backgroundColor: Colors.orange,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(15, 5, 15, 5),
+          child: FloatingActionButton(
+            onPressed: () {
+              changeJokeIndex("next");
+            },
+            child: Icon(Icons.arrow_right_rounded, size: 60),
+            backgroundColor: Colors.orange,
+          ),
+        )
+      ],
+    );
+  }
+}
